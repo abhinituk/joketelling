37986f653e390e2f0495acc41ba9a49b7c33cef7
abhinituk <abhi481.007@gmail.com>
2016-09-10 15:16:49 +0530

    Adding Functional Tests

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -20,15 +20,16 @@ public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, S
     private static MyApi myApiService = null;
     private Context context;
 
+    @SafeVarargs
     @Override
-    protected String doInBackground(Pair<Context, String>... params) {
+    protected final String doInBackground(Pair<Context, String>... params) {
         if(myApiService == null) {  // Only do this once
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
-                    .setRootUrl("http://117.243.180.138:8080/_ah/api/")
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
